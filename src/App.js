5d72ce1ea722487f6f72bbd91d181a3862b7682b
svensk007 <83252177+svensk007@users.noreply.github.com>
2023-02-24 23:33:54 -0600

    scroll wheel thing

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import { motion } from "framer-motion";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header id="head" className="section">
         <motion.h1 animate={{ opacity: [0, 1], y: [75, 0] }}>
-          Satistics are interesting; until you become one.
+          Satistics are interesting; until you{" "}
+          <span className="become-one">become one</span>.
         </motion.h1>
+        <motion.div class="scroll"></motion.div>
       </header>
-      <section></section>
+      <section id="WDYM" className="section">
+        <motion.h1
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ type: "spring", duration: 1, bounce: 0.1 }}
+        >
+          WDYM?
+        </motion.h1>
+      </section>
     </div>
   );
 }
