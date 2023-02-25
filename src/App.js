108213c538174fa7c79de0cb58cdb6477f8de561
svensk007 <83252177+svensk007@users.noreply.github.com>
2023-02-25 14:54:56 -0600

    fancy text gradient

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 function App() {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
   return (
     <div className="App">
       <section id="header" className="section">
         <motion.h1 animate={{ opacity: [0, 1], y: [75, 0] }}>
-          Satistics are interesting; until you{" "}
+          Satistics are fun; until you{" "}
           <span className="become-one">become one</span>.
         </motion.h1>
         <a href="#WDYM">
           <div class="scroll"></div>
         </a>
       </section>
-      <section id="WDYM" className="section">
+      <section ref={ref} id="WDYM" className="section">
         <motion.h1
           initial={{ x: "-100vw" }}
           animate={{ x: 0 }}
