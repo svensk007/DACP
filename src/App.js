37c4cfcd4860dcf9bb56bcbc882f40247b4e0a6d
svensk007 <83252177+svensk007@users.noreply.github.com>
2023-02-24 23:41:51 -0600

    need to make scroll snap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { motion } from "framer-motion";
 function App() {
   return (
     <div className="App">
-      <header id="head" className="section">
+      <section id="header" className="section">
         <motion.h1 animate={{ opacity: [0, 1], y: [75, 0] }}>
           Satistics are interesting; until you{" "}
           <span className="become-one">become one</span>.
         </motion.h1>
-        <motion.div class="scroll"></motion.div>
-      </header>
+        <a href="#WDYM">
+          <div class="scroll"></div>
+        </a>
+      </section>
       <section id="WDYM" className="section">
         <motion.h1
           initial={{ x: "-100vw" }}
