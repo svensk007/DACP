41859c75ccd90a0db45f7b9c0e2eb407cc6bca87
svensk007 <83252177+svensk007@users.noreply.github.com>
2023-02-26 20:39:51 -0600

    got aos working
    (not animate on scroll library)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 function App() {
+  const { ref: WDYM, inView: wdymVisible } = useInView();
+
   return (
     <div className="App">
       <section id="header" className="section">
-        <motion.h1 animate={{ opacity: [0, 1], y: [75, 0] }}>
+        <motion.h1
+          animate={{ opacity: [0, 1], y: [75, 0] }}
+          transition={{ type: "spring", duration: 0.5, bounce: 0.3 }}
+        >
           Satistics are fun; until you{" "}
           <span className="become-one">become one</span>.
         </motion.h1>
         <a href="#WDYM">
-          <div class="scroll"></div>
+          <div className="scroll"></div>
         </a>
       </section>
       <section id="WDYM" className="section">
-        <h1 className="hidden">WDYM?</h1>
+        <h1 ref={WDYM} className={wdymVisible ? "visible" : "hidden"}>
+          WDYM?
+        </h1>
+        <a href="#final">
+          <div className="scroll"></div>
+        </a>
       </section>
       <section id="final" className="section">
         <h1>
