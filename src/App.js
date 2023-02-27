978874c3e622c5bebf5caa66a01da5b89d054891
svensk007 <83252177+svensk007@users.noreply.github.com>
2023-02-26 21:09:31 -0600

    really working the AoS

--- src/App.js
+++ src/App.js
@@ -3,35 +3,42 @@ import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 function App() {
+  // Refs for AoS
   const { ref: WDYM, inView: wdymVisible } = useInView();
+  const { ref: final, inView: finalVisible } = useInView();
 
   return (
     <div className="App">
-      <section id="header" className="section">
-        <motion.h1
-          animate={{ opacity: [0, 1], y: [75, 0] }}
-          transition={{ type: "spring", duration: 0.5, bounce: 0.3 }}
-        >
-          Satistics are fun; until you{" "}
-          <span className="become-one">become one</span>.
-        </motion.h1>
-        <a href="#WDYM">
-          <div className="scroll"></div>
-        </a>
-      </section>
-      <section id="WDYM" className="section">
-        <h1 ref={WDYM} className={wdymVisible ? "visible" : "hidden"}>
-          WDYM?
-        </h1>
-        <a href="#final">
-          <div className="scroll"></div>
-        </a>
-      </section>
-      <section id="final" className="section">
-        <h1>
-          Will you <span className="become-one">become one</span>?
-        </h1>
-      </section>
+      <main>
+        <section id="header" className="section">
+          <motion.h1
+            animate={{ opacity: [0, 1], y: [75, 0] }}
+            transition={{ type: "spring", duration: 0.5, bounce: 0.3 }}
+          >
+            Satistics are fun; until you{" "}
+            <span className="become-one">become one</span>.
+          </motion.h1>
+          <a href="#WDYM">
+            <div className="scroll"></div>
+          </a>
+        </section>
+        <section id="WDYM" className="section">
+          <h1 ref={WDYM} className={wdymVisible ? "visible" : "hidden"}>
+            WDYM?
+          </h1>
+          <a href="#final">
+            <div className="scroll"></div>
+          </a>
+        </section>
+        <section id="final" className="section">
+          <h1
+            ref={final}
+            className={finalVisible ? "final-visible" : "final-hidden"}
+          >
+            Will you <span className="become-one">become one</span>?
+          </h1>
+        </section>
+      </main>
     </div>
   );
 }
